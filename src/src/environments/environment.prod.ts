169748751580e924e5a7b301ac2c3f52adf701a5
Vyacheslav Benedichuk <vbenedichuk@gmail.com>
2020-05-15 21:57:25 +0700

    Urls fixed for docker.

--- src/src/environments/environment.prod.ts
+++ src/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url:'https://auth.localservice',
+  api_url:'https://auth.localservice/auth',
   auth_url:'https://auth.localservice/auth'
 };
